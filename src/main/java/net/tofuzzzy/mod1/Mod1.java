2c82f864d6f10239b06010f7e9b092c70b5497a0
EgilsDev <egils.smidris@gmail.com>
2022-05-29 17:21:06 +0300

    Push 1 but after 2

--- src/main/java/net/tofuzzzy/mod1/Mod1.java
+++ src/main/java/net/tofuzzzy/mod1/Mod1.java
@@ -1,23 +1,15 @@
 package net.tofuzzzy.mod1;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-import java.util.stream.Collectors;
-
 // The value here should match an entry in the META-INF/mods.toml file
+
 // test comment. End me
 @Mod(Mod1.MOD_ID)
 public class Mod1 {
@@ -41,4 +33,5 @@ public class Mod1 {
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
+
 }
